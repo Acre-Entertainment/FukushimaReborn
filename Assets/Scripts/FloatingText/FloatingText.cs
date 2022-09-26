2fb87903d79e4acd511e7672abeb5689da904927
GarotoHonesto <luiginerysilva@outlook.com>
2022-09-26 12:33:50 -0300

    Floating Text FadeIn && FadeOut

--- Assets/Scripts/FloatingText/FloatingText.cs
+++ Assets/Scripts/FloatingText/FloatingText.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class FloatingText : MonoBehaviour
 {
     public MeshRenderer mesh;
+
+    private bool _isShowing;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mesh = GetComponent<MeshRenderer>();
+
+        for (float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            Color c = mesh.material.color;
+            c.a = f;
+            mesh.material.color = c;
+        }
     }
 
     // Update is called once per frame
@@ -19,12 +28,44 @@ public class FloatingText : MonoBehaviour
 
     public void FadeIn()
     {
-        mesh.enabled = true;
+        if (!_isShowing)
+        {
+            StartCoroutine(StartingShow());
+        }
     }
 
     public void FadeOut()
     {
 
-        mesh.enabled = false;
+        if (_isShowing)
+        {
+            StartCoroutine(StartingFade());
+        }
+    }
+
+    IEnumerator StartingShow()
+    {
+        _isShowing = true;
+
+        for (float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            Color c = mesh.material.color;
+            c.a = f;
+            mesh.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    IEnumerator StartingFade()
+    {
+        _isShowing = false;
+
+        for(float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            Color c = mesh.material.color;
+            c.a = f;
+            mesh.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 }
