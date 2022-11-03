9a9e711c8198a41b17ec329e94f13905905608cb
GarotoHonesto <luiginerysilva@outlook.com>
2022-11-03 15:56:25 -0300

    Floating Text

--- Assets/_Fukushima/Scripts/FloatingText/FloatingText.cs
+++ Assets/_Fukushima/Scripts/FloatingText/FloatingText.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 
 public class FloatingText : MonoBehaviour
 {
-    public MeshRenderer mesh;
+    [Header("Animator")]
+    [SerializeField]
+    private Animator _animator;
 
     private bool _isShowing;
     // Start is called before the first frame update
     void Start()
     {
-        mesh = GetComponent<MeshRenderer>();
-
-        for (float f = 1f; f >= -0.05f; f -= 0.05f)
-        {
-            Color c = mesh.material.color;
-            c.a = f;
-            mesh.material.color = c;
-        }
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,7 +25,8 @@ public class FloatingText : MonoBehaviour
     {
         if (!_isShowing)
         {
-            StartCoroutine(StartingShow());
+            _isShowing = true;
+            _animator.SetBool("Fade", true);
         }
     }
 
@@ -39,33 +35,8 @@ public class FloatingText : MonoBehaviour
 
         if (_isShowing)
         {
-            StartCoroutine(StartingFade());
-        }
-    }
-
-    IEnumerator StartingShow()
-    {
-        _isShowing = true;
-
-        for (float f = 0.05f; f <= 1; f += 0.05f)
-        {
-            Color c = mesh.material.color;
-            c.a = f;
-            mesh.material.color = c;
-            yield return new WaitForSeconds(0.05f);
-        }
-    }
-
-    IEnumerator StartingFade()
-    {
-        _isShowing = false;
-
-        for(float f = 1f; f >= -0.05f; f -= 0.05f)
-        {
-            Color c = mesh.material.color;
-            c.a = f;
-            mesh.material.color = c;
-            yield return new WaitForSeconds(0.05f);
+            _isShowing = false;
+            _animator.SetBool("Fade", false);
         }
     }
 }
