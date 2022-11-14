efc3b06b0217f425bc62952d2ea8b39d6e1450e2
Fecyrineu <fcyrineu@gmail.com>
2022-11-14 20:21:26 -0300

    Level 2 final puzzle fix

---
+++ Assets/_Fukushima/Scripts/_Basics/OnThreeBoolsBeingTrue.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnThreeBoolsBeingTrue : MonoBehaviour
+{
+    public bool value1, value2, value3;
+    public UnityEvent unityEvent;
+
+    public void changeValue1(bool newValue)
+    {
+        value1 = newValue;
+    }
+    public void changeValue2(bool newValue)
+    {
+        value2 = newValue;
+    }
+    public void changeValue3(bool newValue)
+    {
+        value3 = newValue;
+    }
+
+    void checkValues()
+    {
+        if(value1 == true && value2 == true && value3 == true)
+        {
+            unityEvent.Invoke();
+        }
+    }
+}
