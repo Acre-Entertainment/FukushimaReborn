78d4076fcbba97687c8ea64df87f4c7a0f2d1fdb
Fecyrineu <fcyrineu@gmail.com>
2022-11-15 19:49:52 -0300

    Update OnThreeBoolsBeingTrue.cs

--- Assets/_Fukushima/Scripts/_Basics/OnThreeBoolsBeingTrue.cs
+++ Assets/_Fukushima/Scripts/_Basics/OnThreeBoolsBeingTrue.cs
@@ -11,14 +11,17 @@ public class OnThreeBoolsBeingTrue : MonoBehaviour
     public void changeValue1(bool newValue)
     {
         value1 = newValue;
+        checkValues()
     }
     public void changeValue2(bool newValue)
     {
         value2 = newValue;
+        checkValues()
     }
     public void changeValue3(bool newValue)
     {
         value3 = newValue;
+        checkValues()
     }
 
     void checkValues()
